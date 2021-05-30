d8a340d340a648ec81f74515db1772d2d8fb0366
Connor Hainje <cmhainje@gmail.com>
2021-05-30 12:39:29 -0400

    Move C90 constant definitions to .c file

--- c90/main.c
+++ c90/main.c
@@ -6,6 +6,28 @@
 
 #include "main.h"
 
+/*** CONSTANTS ***/
+long double hbar = 1.0545718e-34;
+long double c = 299792458;
+long double e = 1.60217662e-19;
+long double G2_init = 1.55e-33 * 0.01*0.01*0.01; // m^3/MeV^2/s
+long double m_n = 939; // MeV
+long double Vp = -0.5 * (1 - 4 * 0.23122);
+long double Ap = -1.2723 / 2;
+long double Vn = -0.5;
+long double An = 1.2723 / 2;
+long double pi = M_PI;
+
+long double e_unit = 1e6 * 1.60217662e-19;                               // 1 MeV       = 1e6 * e
+long double t_unit = 1.0545718e-34 / (1e6 * 1.60217662e-19);             // 6.582e-22 s = hbar / e_unit
+long double l_unit = 1.0545718e-34 / (1e6 * 1.60217662e-19) * 299792458; // 1.973e-13 m = t_unit * c
+
+long double G2 = 1.327817e-22; // converted to natural units = G2_init * t_unit / l_unit**3
+long double m = 939; // MeV
+long double prot = (-0.5 * (1 - 4 * 0.23122)) * (-0.5 * (1 - 4 * 0.23122)) + 5 * (-1.2723 / 2) * (-1.2723 / 2);
+long double neut = (-0.5) * (-0.5) + 5 * (1.2723 / 2) * (1.2723 / 2);
+
+
 /*** UTILITIES ***/
 
 /**
